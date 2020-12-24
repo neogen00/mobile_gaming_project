0413865ff628ebd9a577d28e4c5800b94b23b6c4
Christopher Santos <christopher.e.santos@gmail.com>
2020-12-24 00:02:10 -0800

    added seed.py to seed initial data into videogame database

---
+++ app/api/seed.py
@@ -0,0 +1,56 @@
+"""
+seed.py file used to seed data into psql tables. 
+fixed drop_all_tables() -> drop_records() using TRUNCATE to delete table with foreign key
+added reset_all_primarykey to change primary keys back to 1 every time seed.py is run
+"""
+
+import psycopg2
+
+import src.db as db
+from src.models.game import Game
+from src.models.rating import Rating
+from src.models.earnings import Earnings
+
+# delete/truncate all tables and reset primary keys
+db.drop_all_tables(db.conn, db.cursor)
+db.reset_all_primarykey(db.conn, db.cursor)
+
+
+# seeding games
+amongus = db.save(Game(name = 'Among Us', platform = 'Android', publisher = 'Innersloth LLC', 
+    year_published = 2018, genre = 'Action', game_engine = 'Unity'), db.conn, db.cursor)
+
+roblox = db.save(Game(name = 'Roblox', platform = 'Android', publisher = 'Roblox Corporation', 
+    year_published = 2019, genre = 'Building', game_engine = 'Something Else'), db.conn, db.cursor)
+
+candy = db.save(Game(name = 'Candy Crush Saga', platform = 'Android', publisher = 'King', 
+    year_published = 2015, genre = 'Casual', game_engine = 'Candy Factory'), db.conn, db.cursor)
+
+amongus_ios = db.save(Game(name = 'Among Us', platform = 'iOS', publisher = 'Innersloth LLC', 
+    year_published = 2018, genre = 'Action', game_engine = 'Unity'), db.conn, db.cursor)
+# breakpoint()
+# seeding ratings
+no1 = db.save(Rating(game_id = amongus.id, metacritic = 84, TS_rating = 4.465, 
+    rank_type = 'free', ranking = 1, date_created = '2020-12-01'), db.conn, db.cursor)
+
+no2 = db.save(Rating(game_id = roblox.id, metacritic = 90, TS_rating = 4.48, 
+    rank_type = 'free', ranking = 2, date_created = '2020-12-02'), db.conn, db.cursor)
+
+no3 = db.save(Rating(game_id = candy.id, metacritic = 75, TS_rating = 4.1, 
+    rank_type = 'free', ranking = 3, date_created = '2020-12-03'), db.conn, db.cursor)
+
+no1_ios = db.save(Rating(game_id = amongus_ios.id, metacritic = 84, TS_rating = 4.465, 
+    rank_type = 'free', ranking = 1, date_created = '2020-12-01'), db.conn, db.cursor)
+
+# seeding earnings
+amongus_e = db.save(Earnings(game_id = amongus.id, price = 0.00, inapp = 'yes', 
+    revenue = 35000000, downloads = 100000), db.conn, db.cursor)
+
+roblox_e = db.save(Earnings(game_id = roblox.id, price = 0.01, inapp = 'yes', 
+    revenue = 50000000, downloads = 200000), db.conn, db.cursor)
+
+candy_e = db.save(Earnings(game_id = candy.id, price = 0.50, inapp = 'no', 
+    revenue = 100000000, downloads = 300000), db.conn, db.cursor)
+
+amongus_ios_e = db.save(Earnings(game_id = amongus_ios.id, price = 1.00, inapp = 'yes', 
+    revenue = 2000000, downloads = 75000), db.conn, db.cursor)
