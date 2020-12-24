40e8ddf77ec5762f6a91279122dbc5652dc1aadb
Christopher Santos <christopher.e.santos@gmail.com>
2020-12-24 15:43:57 -0800

    added flask create_app function in src init file and app/run.py to call app

---
+++ app/api/src/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask
+import simplejson as json
+from flask import request
+
+from .db import db
+from .models.game import Game
+from .models.earnings import Earnings
+from .models.rating import Rating
+
+
+def create_app(database='videogame', testing = False, debug = True):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.config.from_mapping(
+        DATABASE=database,
+        DEBUG = debug,
+        TESTING = testing
+    )
+
+    @app.route('/')
+    def root_url():
+        return 'Welcome to the videogame api'
+
+    @app.route('/games')
+    def games():
+        conn = db.get_db()
+        cursor = conn.cursor()
+
+        games = db.find_all(Game, cursor)
+        game_dicts = [game.__dict__ for game in games]
+        return json.dumps(game_dicts, default = str)
+
+    @app.route('/earnings')
+    def earnings():
+        conn = db.get_db()
+        cursor = conn.cursor()
+
+        earnings = db.find_all(Earnings, cursor)
+        earning_dicts = [earning.__dict__ for earning in earnings]
+        return json.dumps(earning_dicts, default = str)
+
+    @app.route('/ratings')
+    def ratings():
+        conn = db.get_db()
+        cursor = conn.cursor()
+
+        ratings = db.find_all(Rating, cursor)
+        rating_dicts = [rating.__dict__ for rating in ratings]
+        return json.dumps(rating_dicts, default = str)
+
+
+    return app
