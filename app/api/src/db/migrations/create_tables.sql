69e9b46e97b50b1ef281a08839bd476e77c49b4f
Christopher Santos <christopher.e.santos@gmail.com>
2020-12-30 14:02:59 -0800

    updated games table year created INT to release_date DATE

--- app/api/src/db/migrations/create_tables.sql
+++ app/api/src/db/migrations/create_tables.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS games (
   name VARCHAR(255) NOT NULL,
   platform VARCHAR(255),
   publisher VARCHAR(255),
-  year_published INTEGER,
+  release_date DATE,
   genre VARCHAR(255),
   game_engine VARCHAR(255)
 );
