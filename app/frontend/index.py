581546fdd0e605fd6674652e7fc90f88eebbaee8
Christopher Santos <christopher.e.santos@gmail.com>
2021-01-09 12:25:50 -0800

    updated streamlit code with more graphs and tables for discovery purposes

--- app/frontend/index.py
+++ app/frontend/index.py
@@ -11,10 +11,15 @@ import plotly.express as px
 
 API_URL_earnings = "http://127.0.0.1:5000/earnings"
 API_URL_games = "http://127.0.0.1:5000/games"
+API_URL_games_with_earnings = "http://127.0.0.1:5000/games/earnings"
 API_URL_ratings = "http://127.0.0.1:5000/ratings"
 
 def get_games():
     response = requests.get(API_URL_games)
+    return response.json()
+    
+def get_games_w_earnings():
+    response = requests.get(API_URL_games_with_earnings)
     return response.json()
 
 def get_earnings():
@@ -31,6 +36,7 @@ def game_revenue(earnings):
 def game_downloads(downloads):
     return [download['downloads'] for download in downloads]
 
+# earnings = st.sidebar.slider(min_value = 1, max_value = 2, step = 1, label = 'price')
 st.header('earnings')
 earnings = get_earnings()
 games_list = get_games()
@@ -40,23 +46,29 @@ st.write("These are your downloads ", x)
 y = game_revenue(earnings)
 st.write("These are your revenues", y)
 
-
 dF = pd.DataFrame(earnings)
-
-games=['Among Us Android', 'Roblox Android', 'Candy Crush Saga Android', 'Among Us iOS']
-
-fig = px.scatter(dF,x='downloads',y='revenue',hover_name=games,color="inapp",
-    title="Game revenue vs downloads with inapp info")
+fig = px.scatter(dF,x='downloads',y='revenue',hover_name='game_id',
+  color='inapp',
+  title="Game revenue vs downloads with inapp info")
 st.plotly_chart(fig)
 
-fig2 = px.bar(dF,x=games,y='downloads')
-st.plotly_chart(fig2)
+fig_b = px.scatter(dF,x='downloads',y='revenue',hover_name='game_id',
+  color='price',
+  title="Game revenue vs downloads with price info")
+st.plotly_chart(fig_b)
 
-fig3 = go.Figure([go.Bar(x=games, y=y)])
+st.write("Revenue Per Download")
+dF['RPD'] = dF['revenue'] / dF['downloads']
+dF = dF.sort_values(by='RPD', ascending=False)
+fig2 = go.Figure([go.Bar(y=dF['RPD'])])
+
+st.plotly_chart(fig2)
+st.table(dF)
+
+fig3 = go.Figure([go.Bar(y=y)])
 st.plotly_chart(fig3)
 
 game_df = pd.DataFrame(games_list)
-
 st.table(game_df)
 
 st.table(dF)
@@ -64,5 +76,4 @@ st.table(dF)
 st.header('ratings')
 ratings = get_ratings()
 ratings_df = pd.DataFrame(ratings)
-
 st.table(ratings_df)
