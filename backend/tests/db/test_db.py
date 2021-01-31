3b9ee6b9e54323328719f8ee549249980f55797c
Christopher Santos <christopher.e.santos@gmail.com>
2021-01-31 01:48:52 -0800

    added test_db.py for update column feature

--- backend/tests/db/test_db.py
+++ backend/tests/db/test_db.py
@@ -5,7 +5,6 @@ import pytest
 import api.src.adapters as adapters
 import api.src.db.db as db
 import api.src.models as models
-# from  tests.data.builder_data import (s3d_rating, s3d_earnings, s3d_details, s3d_record_date, s3d_rank_type, s3d_input, s3d_lower, s3d_higher, TS_details, amongus_ios)
 
 
 @pytest.fixture(scope = 'module')
@@ -19,15 +18,6 @@ def test_conn():
     db.drop_all_tables(test_conn, cursor)
     db.reset_all_primarykey(test_conn, cursor)
 
-
-# def build_record(test_conn, cursor):
-#     r1 = {"id": 1, "game_id": 1, "metacritic": 84, "TS_rating": 4.43, "rank_type": "top free", "ranking": 1, "date_created": "2021-01-01"}
-#     g1 = {"id": 1, "name": "Among Us", "platform": "android", "publisher": "Innersloth LLC", "release_date": "2018-07-25", "genre": "action", "game_engine": "Unity"}
-#     e1 = {"id": 1, "game_id": 1, "price": 0.0, "inapp": True, "shows_ads": False, "revenue": 2000000, "downloads": 29000000}
-#     game = db.save(models.Game(**g1), test_conn, cursor)
-#     rating = db.save(models.Rating(**r1), test_conn, cursor)
-#     earnings = db.save(models.Earnings(**e1), test_conn, cursor)
-#     return game, rating, earnings
 
 def test_update_column_for_games_table(test_conn):
     test_cursor = test_conn.cursor()
@@ -46,9 +36,7 @@ def test_update_column_for_games_table(test_conn):
 
 def test_update_column_for_earnings_table(test_conn):
     test_cursor = test_conn.cursor()
-    # g1 = {"id": 1, "name": "Among Us", "platform": "android", "publisher": "Innersloth LLC", "release_date": "2018-07-25", "genre": "action", "game_engine": "Unity"}
     e1 = {"id": 1, "game_id": 1, "price": 0.0, "inapp": True, "shows_ads": False, "revenue": 2000000, "downloads": 29000000}
-    # game = db.save(models.Game(**g1), test_conn, test_cursor)
     earnings = db.save(models.Earnings(**e1), test_conn, test_cursor)
     
     earnings.downloads = 23000000
