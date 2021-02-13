b04dabd518bffcd183a2259b8b670cbc834ea526
Christopher Santos <christopher.e.santos@gmail.com>
2021-02-13 00:33:49 -0800

    1st try to use click for deploying backend

---
+++ backend/manage.py
@@ -0,0 +1,25 @@
+import sys
+from flask.cli import FlaskGroup
+from src import create_app
+from src.adapters.run_adapters import RequestAndBuild
+import click
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+days=1
+limit=10
+
+@cli.command('build_games')
+@click.argument('record_start')
+@click.option('--days', type=click.INT)
+@click.option('--limit', type=click.INT)
+def build_games(record_start, days, limit):
+    # "2020-12-20", 1, 10
+    runner = RequestAndBuild()
+    runner.run(record_start, days, limit)
+    print(record_start, days, limit)
+
+
+if __name__ == "__main__":
+    cli()
