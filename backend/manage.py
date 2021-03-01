1dd9ce7b20d4d23912d8deed8ff4d0b5d54b9826
Christopher Santos <christopher.e.santos@gmail.com>
2021-02-28 23:56:41 -0800

    changed variable option to arguments, int numbers

--- backend/manage.py
+++ backend/manage.py
@@ -12,12 +12,12 @@ limit=10
 
 @cli.command('build_games')
 @click.argument('record_start')
-@click.option('--days', type=click.INT)
-@click.option('--limit', type=click.INT)
+@click.argument('days')
+@click.argument('limit')
 def build_games(record_start, days, limit):
     # "2020-12-20", 1, 10
     runner = RequestAndBuild()
-    runner.run(record_start, days, limit)
+    runner.run(record_start, int(days), int(limit))
     print(record_start, days, limit)
 
 
